fbfec2f9dd891bb0d64ff67ff3fb1294e0216fe1
vishnuprasadkmp <vishnuprasadkmp@gmail.com>
2025-07-04 12:24:53 +0530

    vite updations

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Enables access via network
-    port: parseInt(process.env.PORT) || 5173, // Uses .env or fallback
+    host: '0.0.0.0', // Allow external access
+    port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: ['photographerportfolio-ioua.onrender.com'], // ✅ Fix for blocked host
   },
-})
+});
